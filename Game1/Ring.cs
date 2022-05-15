d4bf1728fcb77c2bbdbaf24f8949235c621b4a31
Jonas Puksta <jonaspuksta@gmail.com>
2022-05-15 15:17:05 +0300

    fixed compilation

--- Game1/Ring.cs
+++ Game1/Ring.cs
@@ -1,23 +1,23 @@
-﻿using Game1.Shapes;
+﻿//using Game1.Shapes;
 
-namespace Game1
-{
-    public class Ring : Shape
-    {
-        public interface IParamsWithInnerRadius
-        {
-            public MyVector2 Center { get; }
+//namespace Game1
+//{
+//    public class Ring : Shape
+//    {
+//        public interface IParamsWithInnerRadius
+//        {
+//            public MyVector2 Center { get; }
 
-            public UDouble InnerRadius { get; }
-        }
+//            public UDouble InnerRadius { get; }
+//        }
 
-        public interface IParamsWithOuterRadius
-        {
-            public MyVector2 Center { get; }
+//        public interface IParamsWithOuterRadius
+//        {
+//            public MyVector2 Center { get; }
 
-            public UDouble OuterRadius { get; }
-        }
+//            public UDouble OuterRadius { get; }
+//        }
 
-        //continue here
-    }
-}
+//        //continue here
+//    }
+//}

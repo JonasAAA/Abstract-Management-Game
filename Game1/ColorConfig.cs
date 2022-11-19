3e853b839470c75fe38ed6f8385199dc7360ea3c
Jonas Puksta <jonaspuksta@gmail.com>
2022-11-19 10:46:54 +0200

    changed a couple of colors and moved their initialization to constructor

--- Game1/ColorConfig.cs
+++ Game1/ColorConfig.cs
@@ -5,22 +5,45 @@
     {
         public readonly Color
             // world colors
-            Res0Color = C.ColorFromRGB(rgb: 0x00754D),
-            Res1Color = C.ColorFromRGB(rgb: 0x00632D),
-            cosmosBackgroundColor = C.ColorFromRGB(rgb: 0x000E24),
-            starColor = C.ColorFromRGB(rgb: 0xB0FF00),
-            houseIndustryColor = Color.Yellow,
-            linkTravellerColor = Color.Black,
+            Res0Color,
+            Res1Color,
+            cosmosBackgroundColor,
+            starColor,
+            houseIndustryColor,
+            linkTravellerColor,
             
             // UI colors
-            mouseOnColor = Color.Yellow,
-            UIBackgroundColor = C.ColorFromRGB(rgb: 0x124A5E),
-            textColor = C.ColorFromRGB(rgb: 0xADE38F),
-            almostWhiteColor = Color.Aqua,
-            buttonColor = Color.Aqua,
-            selectedButtonColor = C.ColorFromRGB(rgb: 0x124A5E),// Color.White,
-            deselectedButtonColor = Color.Gray,
-            tooltipBackgroundColor = Color.LightPink,
+            mouseOnColor,
+            UIBackgroundColor,
+            textColor,
+            almostWhiteColor,
+            buttonColor,
+            selectedButtonColor,
+            deselectedButtonColor,
+            tooltipBackgroundColor,
+            deleteButtonColor;
+
+        // colors are assigned in constructor to allow referencing previously defined colors
+        public ColorConfig()
+        {
+            // world colors
+            Res0Color = C.ColorFromRGB(rgb: 0x00754D);
+            Res1Color = C.ColorFromRGB(rgb: 0x00632D);
+            cosmosBackgroundColor = C.ColorFromRGB(rgb: 0x000E24);
+            starColor = C.ColorFromRGB(rgb: 0xB0FF00);
+            houseIndustryColor = Color.Yellow;
+            linkTravellerColor = Color.Black;
+
+            // UI colors
+            mouseOnColor = Color.Yellow;
+            UIBackgroundColor = C.ColorFromRGB(rgb: 0x124A5E);
+            textColor = C.ColorFromRGB(rgb: 0xADE38F);
+            almostWhiteColor = Color.Aqua;
+            buttonColor = Color.Aqua;
+            selectedButtonColor = C.ColorFromRGB(rgb: 0x03645);
+            deselectedButtonColor = UIBackgroundColor;
+            tooltipBackgroundColor = Color.LightPink;
             deleteButtonColor = Color.Red;
+        }
     }
 }

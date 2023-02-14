64024e2003d1a584b1776eed095c2cbea9eedce1
Jonas Puksta <jonaspuksta@gmail.com>
2023-02-14 09:46:00 +0200

    added commented out code for EnergyRecipe

--- Game1/Resources/ResRecipe.cs
+++ Game1/Resources/ResRecipe.cs
@@ -17,9 +17,9 @@
 
         private ResRecipe(ResAmounts ingredients, ResAmounts results)
         {
+            Debug.Assert(AreValid(ingredients: ingredients, results: results));
             this.ingredients = ingredients;
             this.results = results;
-            Debug.Assert(AreValid(ingredients: ingredients, results: results));
         }
 
         private static bool AreValid(ResAmounts ingredients, ResAmounts results)
@@ -32,3 +32,34 @@
             => scalar * resRecipe;
     }
 }
+
+// TODO: could create energy recipe with something like
+//namespace Game1.Resources
+//{
+//    public class EnergyRecipe<TSourceAmount, TDestinAmount>
+//        where TSourceAmount : struct, IFormOfEnergy<TSourceAmount>
+//        where TDestinAmount : struct, IFormOfEnergy<TDestinAmount>
+//    {
+//        public static EnergyRecipe<TSourceAmount, TDestinAmount>? Create(TSourceAmount ingredients, TDestinAmount results)
+//        {
+//            if (AreValid(ingredients: ingredients, results: results))
+//                return new(ingredients: ingredients, results: results);
+//            return null;
+//        }
+
+//        public readonly TSourceAmount ingredients;
+//        public readonly TDestinAmount results;
+
+//        private EnergyRecipe(TSourceAmount ingredients, TDestinAmount results)
+//        {
+//            "Do I make stuff use this instead of current energy conversion methods?
+//            Debug.Assert(AreValid(ingredients: ingredients, results: results));
+//            this.ingredients = ingredients;
+//            this.results = results;
+//        }
+
+//        private static bool AreValid(TSourceAmount ingredients, TDestinAmount results)
+//            => (Energy)ingredients == (Energy)results;
+//    }
+//}
+

b6d6f1d21226f40db40b8a1f446164c82a25d8ba
Jonas Puksta <jonaspuksta@gmail.com>
2023-11-03 10:53:04 +0200

    added comment

--- Game1/Industries/MechProdStats.cs
+++ Game1/Industries/MechProdStats.cs
@@ -1,5 +1,8 @@
 ﻿namespace Game1.Industries
 {
+    /// <summary>
+    /// <paramref name="ReqWatts"/> doesn't use type ElectricalEnergy as that can only be integer
+    /// </summary>
     [Serializable]
     public readonly record struct MechProdStats(UDouble ReqWatts, AreaDouble ProducedAreaPerSec);
 }

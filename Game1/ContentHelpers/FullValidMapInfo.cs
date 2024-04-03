4a6d60a2ef451bdc305e3db6069bfe5ad07a0247
Jonas Puksta <jonaspuksta@gmail.com>
2024-04-03 16:14:23 +0300

    since the game assumes that the graph is connected, I now check for that

--- Game1/ContentHelpers/FullValidMapInfo.cs
+++ Game1/ContentHelpers/FullValidMapInfo.cs
@@ -19,11 +19,86 @@ namespace Game1.ContentHelpers
             //   select new FullValidMapInfo(cosmicBodies, links, startingInfo);
             => Result.Lift
             (
-                func: (arg1, arg2, arg3) => new FullValidMapInfo(cosmicBodies: arg1.ToEfficientReadOnlyCollection(), links: arg2.ToEfficientReadOnlyCollection(), startingInfo: arg3),
+                func: (arg1, arg2, arg3) =>
+                {
+                    var cosmicBodies = arg1.ToEfficientReadOnlyCollection();
+                    var links = arg2.ToEfficientReadOnlyCollection();
+                    return FindDisconnectedCosmicBodies(cosmicBodies: cosmicBodies, links: links) switch
+                    {
+                        var (cosmicBodyA, cosmicBodyB) => new(errors: new($"It must be possible to travel between any two cosmic bodies using links.\nThis is not the case for cosmic bodies named {cosmicBodyA} and {cosmicBodyB}")),
+                        null => new Result<FullValidMapInfo, TextErrors>(ok: new FullValidMapInfo(cosmicBodies: cosmicBodies, links: links, startingInfo: arg3))
+                    };
+                },
                 arg1: mapInfo.CosmicBodies.SelectMany(FullValidCosmicBodyInfo.Create),
                 arg2: mapInfo.Links.SelectMany(FullValidLinkInfo.Create),
                 arg3: FullValidStartingInfo.Create(startingInfo: mapInfo.StartingInfo)
+            ).SelectMany(fullValidMapInfo => fullValidMapInfo);
+
+        private static (string cosmicBodyA, string cosmicBodyB)? FindDisconnectedCosmicBodies(EfficientReadOnlyCollection<FullValidCosmicBodyInfo> cosmicBodies, EfficientReadOnlyCollection<FullValidLinkInfo> links)
+        {
+            // Disjoint set union algorithm
+            var cosmicBodyToRepr = cosmicBodies.ToDictionary
+            (
+                keySelector: cosmicBody => cosmicBody.Name,
+                elementSelector: cosmicBody => cosmicBody.Name
             );
+            var setReprToSetCount = cosmicBodies.ToDictionary
+            (
+                keySelector: cosmicBody => cosmicBody.Name,
+                elementSelector: cosmicBody => 1ul
+            );
+            foreach (var link in links)
+            {
+                var reprA = FindRepresentative(cosmicBody: link.From);
+                var reprB = FindRepresentative(cosmicBody: link.To);
+                if (reprA == reprB)
+                    continue;
+                if (setReprToSetCount[reprA] < setReprToSetCount[reprB])
+                    (reprA, reprB) = (reprB, reprA);
+                // it would be enough to simply state that reprB is represented by reprA, but this way it's a tiny bit more efficient
+                SetNewRepresentative(cosmicBody: link.From, newRepr: reprA);
+                SetNewRepresentative(cosmicBody: link.To, newRepr: reprA);
+                cosmicBodyToRepr[reprB] = reprA;
+                setReprToSetCount[reprA] += setReprToSetCount[reprB];
+                // This removing in not necessary, strictly speaking
+                setReprToSetCount.Remove(reprB);
+            }
+
+            string? overallRepr = null;
+            foreach (var cosmicBody in cosmicBodies)
+            {
+                var repr = FindRepresentative(cosmicBody: cosmicBody.Name);
+                SetNewRepresentative(cosmicBody: cosmicBody.Name, newRepr: repr);
+                overallRepr ??= repr;
+                if (overallRepr != repr)
+                    return (cosmicBodyA: overallRepr, cosmicBodyB: repr);
+            }
+
+            return null;
+
+            string FindRepresentative(string cosmicBody)
+            {
+                while (true)
+                {
+                    var repr = cosmicBodyToRepr[cosmicBody];
+                    if (repr == cosmicBody)
+                        return cosmicBody;
+                    cosmicBody = repr;
+                }
+            }
+
+            string SetNewRepresentative(string cosmicBody, string newRepr)
+            {
+                while (true)
+                {
+                    cosmicBodyToRepr[cosmicBody] = newRepr;
+                    var repr = cosmicBodyToRepr[cosmicBody];
+                    if (repr == cosmicBody)
+                        return cosmicBody;
+                    cosmicBody = repr;
+                }
+            }
+        }
 
         public EfficientReadOnlyCollection<FullValidCosmicBodyInfo> CosmicBodies { get; }
         public EfficientReadOnlyCollection<FullValidLinkInfo> Links { get; }
